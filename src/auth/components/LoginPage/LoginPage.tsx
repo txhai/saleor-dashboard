59dcf62d8586bee64b8346a292fef2eed6a7f7c3
txhai <haitrandocs@gmail.com>
2020-11-28 00:12:34 -0800

    Update recaptcha description

--- src/auth/components/LoginPage/LoginPage.tsx
+++ src/auth/components/LoginPage/LoginPage.tsx
@@ -125,6 +125,17 @@ const LoginCard: React.FC<LoginCardProps> = props => {
                 type="submit"
                 data-test="submit"
               >
+                <small className="text-muted">
+                  This site is protected by reCAPTCHA and the Google
+                  <a href="https://policies.google.com/privacy">
+                    Privacy Policy
+                  </a>{" "}
+                  and
+                  <a href="https://policies.google.com/terms">
+                    Terms of Service
+                  </a>{" "}
+                  apply.
+                </small>
                 <FormattedMessage defaultMessage="Login" description="button" />
               </Button>
             </div>
